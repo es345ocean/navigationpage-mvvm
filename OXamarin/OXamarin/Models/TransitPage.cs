d50ac3258f598163798b513c4a7973d38d15696c
karimata110611 <karimata1106@gmail.com>
2017-05-05 19:51:57 +0900

    NavigationPage MVVM

---
+++ OXamarin/OXamarin/Models/TransitPage.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+
+namespace OXamarin.Models
+{
+    public class TransitPage : BindableBase
+    {
+        private string _categoryID;
+
+        public string CategoryID
+        {
+            get { return _categoryID; }
+            set { SetProperty(ref _categoryID, value); }
+        }
+
+        private string _categoryName;
+        public string CategoryName
+        {
+            get { return _categoryName; }
+            set { SetProperty(ref _categoryName, value); }
+        }
+    }
+}
---
+++ OXamarin/OXamarin/ViewModels/InputPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using OXamarin.Models;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace OXamarin.ViewModels
+{
+    public class InputPageViewModel : BindableBase, INavigationAware
+    {
+        #region 定数
+
+        public static readonly string InputKey = "InputKey";
+
+        #endregion
+
+        #region フィールド
+
+        private string _title;
+        private string _categoryID;
+
+        #endregion
+
+        #region プロパティ
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public string CategoryID
+        {
+            get { return _categoryID; }
+            set { SetProperty(ref _categoryID, value); }
+        }
+
+        #endregion
+
+        #region メソッド
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+            //NavigationParameterに「InputKey」をキーとした
+            //パラメーターを持っているかどうかの確認
+            if (parameters.ContainsKey(InputKey))
+            {
+                var selectedItem = (TransitPage)parameters[InputKey];
+                Title = selectedItem.CategoryName;
+                CategoryID = selectedItem.CategoryID;
+            }
+        }
+
+        #endregion
+    }
+}
